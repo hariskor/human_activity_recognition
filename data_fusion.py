c0e85535456c4336d77a669f70aa5aa2490d6355
hariskor <kormenidisx@gmail.com>
2022-01-29 12:56:10 +0100

    2nd version of data_fusion. LEft the files splitted in toder to have faster execution time

--- data_fusion.py
+++ data_fusion.py
@@ -4,11 +4,11 @@ import os
 import sys
 from datetime import datetime
 
-def save_data(x, y):
+def save_data(x, y,index):
         dir_path = os.path.dirname(os.path.realpath(__file__))
 
-        np.savetxt(dir_path+'/combined_data/x.csv',x,delimiter=';',fmt='%s')
-        np.savetxt(dir_path+'/combined_data/y.csv',y,delimiter=';')
+        np.savetxt(dir_path+'/combined_data/x_'+str(index)+'.csv',x,delimiter=';',fmt='%s')
+        np.savetxt(dir_path+'/combined_data/y_'+str(index)+'.csv',y,delimiter=';')
         print('data saved')
         return
 
@@ -34,51 +34,47 @@ def createDFNode(data1x,data2x,i,index):
     return data
 
 
-def read_data(sensor):
+def read_data(sensor,index):
 
-        dir = os.listdir('data/'+sensor)
-        dFramesX = []
-        dFramesY = []
-        for i in range(int(len(dir) / 2)):  # half is X half is Y
-            dFramesX.append(pd.read_csv('data/'+sensor+'/'+sensor+'_X_0' + str(i + 1) + '.csv'))
-            dFramesY.append(pd.read_csv('data/'+sensor+'/'+sensor+'_Y_0' + str(i + 1) + '.csv'))
+    dFrameX = pd.read_csv('data/' + sensor + '/' + sensor +'_X_0' + index + '.csv')
+    dFrameY = pd.read_csv('data/'+sensor+'/'+sensor+'_Y_0' + index + '.csv')
 
-        dataX = pd.concat(dFramesX, axis=0, ignore_index=True)
-        dataY = pd.concat(dFramesY, axis=0, ignore_index=True)
-
-        # print(len(dataX.index))
-
-        for i in range(0,20):
-            # print(dataX.at[i, 'Timestamp'])
-            dataX.at[i, 'Timestamp'] = pd.Timestamp(dataX.at[i,'Timestamp'])
-
-        dataX.at[0, 'Timestamp'] = pd.Timestamp(dataX.at[0,'Timestamp'])
-
+    for j in range(1,len(dFrameX)):
+        dFrameX.at[j, 'Timestamp'] = pd.Timestamp(dFrameX.at[j,'Timestamp'])
+    dFrameX.at[0, 'Timestamp'] = pd.Timestamp(dFrameX.at[0,'Timestamp'])
+    
         # dataX = dataX.iloc[1:500]
         # dataY = dataY.iloc[1:500]
-        return dataX, dataY
+    return dFrameX, dFrameY
 
 def combine_data(data1x,data1y,data2x,data2y):
 
+    datax = np.empty(shape=[0,15])
     datax = pd.DataFrame()
     datay = pd.DataFrame()
-    for i in range(1,len(data1x)):
+    for i in range(0,len(data1x)):
         min = sys.maxsize
         count = 0
         index = None
         index = None
         # print('i',i)
-        for j in range(1,len(data2x)):
+        for j in range(int(i*2.56),len(data2x)):
             # print('j',j)
             delta = abs(pd.Timedelta(pd.Timestamp(data1x.at[i,'Timestamp']) - pd.Timestamp((data2x.at[j,'Timestamp']))).delta)
             if(min >= delta):
                 min = delta
                 index = j
+            else:
+                count+= 1
+            if count ==3 :
+                break
 
         if index == None:
+            print('no index')
             continue
 
         if(data1y.at[i,'label'] != data2y.at[index,'label']):
+            print('wrong label')
             continue
 
         dfNode = createDFNode(data1x,data2x,i,index)
@@ -90,6 +86,9 @@ def combine_data(data1x,data1y,data2x,data2y):
             dfNode,
             ignore_index=True
         )
+        # if(i!=0):
+            # print('i/j',j/i,'min',min)
+        # print('.', sep=' ', end='', flush=True)
 
      #dataset 1 should be the one with the lower frequency
      #ankle is 100hz, therefore it comes first
@@ -100,16 +99,19 @@ def combine_data(data1x,data1y,data2x,data2y):
 # print(pd.Timestamp('2020-07-30 12:11:49.117'))
 # print(pd.Timestamp('2020-07-30T12:11:49.3100000'))
 print('reading data...')
-ankleX,ankleY = read_data('ankle')
-wristX,wristY = read_data('wrist')
 
-print('')
-print('combining data...')
-dataX,dataY = combine_data(ankleX,ankleY,wristX,wristY)
+for i in range(1,8):
+    ankleX,ankleY = read_data('ankle',str(i))
+    wristX,wristY = read_data('wrist',str(i))
 
-print('')
-print('saving data...')
-save_data(dataX,dataY)
+    print('')
+    print('combining data...')
+    dataX,dataY = combine_data(ankleX,ankleY,wristX,wristY)
+
+    print('')
+    print('saving data...')
+    save_data(dataX,dataY,i)
+
 
 # print(dataX)
 # print(dataY)
